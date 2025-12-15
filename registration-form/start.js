652f3cd19aa41895d83e740979d8cacab0f8164c
Atai824 <a.toktosunov.8207@westcliff.edu>
2025-12-14 17:31:48 -0800

    Done

--- registration-form/start.js
+++ registration-form/start.js
@@ -1,10 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(process.env.DATABASE);
 
 mongoose.connection
   .on('open', () => {
